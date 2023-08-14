550f94c53fb9c22cb53d556477d15ed4e4948780
Jeroen van der Meulen <jeroennathan.vandermeulen@gmail.com>
2023-08-14 23:24:43 +0200

    add env to prevent fail

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -2,6 +2,15 @@ import {getRepoNames} from '../src/main'
 
 describe('getRepoNames', () => {
     it('should fetch repository names for the given organization', async () => {
+        process.env = {
+            GH_ORG: 'myorg',
+            GH_API_KEY: 'apikey',
+            AWS_BUCKET_NAME: 'bucket',
+            AWS_BUCKET_REGION: 'region',
+            AWS_ACCESS_KEY: 'accesskey',
+            AWS_SECRET_KEY: 'secretkey'
+        }
+
         const organization = 'testOrg'
 
         const repoNames = await getRepoNames(organization)
@@ -10,6 +19,15 @@ describe('getRepoNames', () => {
     })
 
     it('should handle errors when fetching repository names', async () => {
+        process.env = {
+            GH_ORG: 'myorg',
+            GH_API_KEY: 'apikey',
+            AWS_BUCKET_NAME: 'bucket',
+            AWS_BUCKET_REGION: 'region',
+            AWS_ACCESS_KEY: 'accesskey',
+            AWS_SECRET_KEY: 'secretkey'
+        }
+
         const organization = 'testOrgthatdoesnotexist'
 
         await expect(getRepoNames(organization)).rejects.toThrowError(
